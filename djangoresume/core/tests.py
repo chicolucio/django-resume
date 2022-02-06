5e093283dd9ea094288a9841f2328af16e35a2e5
Francisco Bustamante <chicolucio@gmail.com>
2022-02-06 19:45:30 -0300

    tests without DB and reverse

--- djangoresume/core/tests.py
+++ djangoresume/core/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 import pytest
-from django.urls import reverse
 
 _test_case = TestCase()
 
@@ -8,32 +7,10 @@ assert_contains = _test_case.assertContains
 assert_not_contains = _test_case.assertNotContains
 assert_template = _test_case.assertTemplateUsed
 
-# class HomeTest(TestCase):
-#     """
-#     Test home
-#     """
-
-#     def setUp(self):
-#         self.response = self.client.get('/')
-
-#     def test_get(self):
-#         """GET / must return status code 200"""
-#         self.assertEqual(200, self.response.status_code)
-
-#     def test_template(self):
-#         """Must use core/home.html"""
-#         self.assertTemplateUsed(self.response, 'core/home.html')
-
-#     def test_html(self):
-#         """HTML must contain"""
-#         self.assertContains(self.response, 'Francisco Bustamante')
-#         self.assertContains(self.response, 'Python developer')
-#         self.assertContains(self.response, '<article', 8)
-
 
 @pytest.fixture
-def resp(client, db):
-    resp = client.get(reverse('home'))
+def resp(client):
+    resp = client.get('/')
     return resp
 
 
